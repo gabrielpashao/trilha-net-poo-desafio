68ae62f29da2da619b99923efa9b681e7478077a
Gabriel Paixão <gabriel_paixao2015@hotmail.com>
2025-05-17 18:11:52 -0300

    Resolução do desafio de abstração e encapsulamento

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("87888501", "Lumia", "123456789", 256);
+Smartphone iphone = new Iphone("88871580", "Iphone 11", "987654321", 128);
+
+Console.Clear();
+
+Console.WriteLine("Iniciando o Smartphone Nokia...");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("ZapZap");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iniciando o Smartphone Iphone...");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
